e1a8104346c7a0c3e557602c38b967e18ad0472a
Michal <11michi11@gmail.com>
2020-09-13 17:10:56 +0200

    Remove main from the UserService

--- UserService/src/main/java/com/filmmaster/userservice/security/PwdEncrypt.java
+++ UserService/src/main/java/com/filmmaster/userservice/security/PwdEncrypt.java
@@ -6,9 +6,9 @@ import java.util.List;
 
 public class PwdEncrypt {
 
-    public static void main(String[] args) {
-        List<String> list = List.of("pwd", "password", "secret");
-
-        list.forEach(pwd -> System.out.println(new BCryptPasswordEncoder().encode(pwd)));
-    }
+//    public static void main(String[] args) {
+//        List<String> list = List.of("pwd", "password", "secret");
+//
+//        list.forEach(pwd -> System.out.println(new BCryptPasswordEncoder().encode(pwd)));
+//    }
 }
