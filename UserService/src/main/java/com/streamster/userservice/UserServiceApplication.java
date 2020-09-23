5d440c7b2d59442d5885888ce87f77942730c0a6
Unknown <michalekmato@gmail.com>
2020-09-23 15:19:45 +0200

    build gradle files and package names updated

---
+++ UserService/src/main/java/com/streamster/userservice/UserServiceApplication.java
@@ -0,0 +1,27 @@
+package com.streamster.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableAuthorizationServer
+@EnableScheduling
+@EnableSwagger2
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
