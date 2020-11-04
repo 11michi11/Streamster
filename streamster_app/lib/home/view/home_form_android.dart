8c527624fd4940371c1f9c282e26417812221ad5
Matej <michalekmato@gmail.com>
2020-11-04 15:54:41 +0100

    Fixed bugs related to register

--- streamster_app/lib/home/view/home_form_android.dart
+++ streamster_app/lib/home/view/home_form_android.dart
@@ -26,8 +26,7 @@ class _HomeFormAndroidState extends State<HomeFormAndroid> {
             shape: BoxShape.circle,
             image: DecorationImage(
                 fit: BoxFit.fill,
-                image: NetworkImage(
-                    "https://scontent-cph2-1.xx.fbcdn.net/v/t31.0-8/18358881_1293777014069016_6493560286323388419_o.jpg?_nc_cat=104&_nc_sid=09cbfe&_nc_ohc=ObxWnFMAMGgAX9R_SfZ&_nc_ht=scontent-cph2-1.xx&oh=0f5fa0c0ea02b3df77eecd4fc8ab548e&oe=5FAB318A"))));
+                image: AssetImage("images/person_grey_icon.png"))));
   }
 
   @override
